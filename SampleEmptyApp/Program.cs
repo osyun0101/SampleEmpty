d1e51b1ae445bbdfa903a62d7b8eef0f3cb0b6e8
natyudon <61405828+natyudon@users.noreply.github.com>
2021-03-10 21:49:33 +0900

    commit

--- SampleEmptyApp/Program.cs
+++ SampleEmptyApp/Program.cs
@@ -13,14 +13,16 @@ namespace SampleEmptyApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Helloworldll");
+            /*CreateHostBuilder(args).Build().Run();*/
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        /* public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        */
     }
 }
