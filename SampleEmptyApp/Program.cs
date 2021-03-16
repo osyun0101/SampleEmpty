6c06684d36b0fe03b2fad7c79335da09039e5720
natyudon <61405828+natyudon@users.noreply.github.com>
2021-03-16 13:00:12 +0900

    コンストラクタ問題

--- SampleEmptyApp/Program.cs
+++ SampleEmptyApp/Program.cs
@@ -4,19 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SampleEx101
+namespace SampleEmptyApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Person p1, p2;
-            p1 = new Person();  //  引数なしのコンストラクタ
-            p2 = new Person("太田隆", 29); //  引数ありのコンストラクタ
-            p1.Name = "斉藤花子";
-            p1.Age = 18;
-            p1.ShowAgeAndName();
-            p2.ShowAgeAndName();
+            Sample s = new Sample();
+            s = null;
+            GC.Collect();
         }
     }
 }
---
+++ SampleEmptyApp/Sample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEmptyApp
+{
+    class Sample
+    {
+        //  コンストラクタ
+        public Sample()
+        {
+            Console.WriteLine("コンストラクタ");
+        }
+        //  デストラクタ
+        ~Sample()
+        {
+            Console.WriteLine("デストラクタ");
+        }
+    }
+}
