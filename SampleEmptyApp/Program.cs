9f365acdfd6d05d4b186ed4ea7902ee2b7a396fe
natyudon <61405828+natyudon@users.noreply.github.com>
2021-03-20 23:06:13 +0900

    コレクション

--- SampleEmptyApp/Program.cs
+++ SampleEmptyApp/Program.cs
@@ -10,15 +10,17 @@ namespace SampleEmptyApp
     {
         static void Main(string[] args)
         {
-            Dummy d = new Dummy();
-            IFuncs1 i1 = (IFuncs1)d;
-            IFuncs2 i2 = (IFuncs2)d;
-            //  i1のメソッドを利用
-            i1.Func1();
-            i1.Func2();
-            //  i2のメソッドを利用
-            i2.Func2();
-            i2.Func3();
+            List<int> a = new List<int>();
+            //  値を順に挿入
+            a.Add(3);
+            a.Add(2);
+            a.Add(1);
+            //  1番目に4を挿入
+            a.Insert(1, 4);
+            for (int i = 0; i < a.Count; i++)
+            {
+                Console.WriteLine("a[{0}]={1} ", i, a[i]);
+            }
         }
     }
 }
